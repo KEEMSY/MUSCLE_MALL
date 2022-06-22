663f4ecc5a134c525315029f0e103b768980c060
KEEMSY <phzzz@naver.com>
2022-06-22 19:29:28 +0900

    add: 회원가입 기능 추가

--- userapp/views.py
+++ userapp/views.py
@@ -9,18 +9,22 @@ from userapp.serializers import UserSerializer
 
 
 class UserApiView(APIView):
+    # 사용자 정보 조회
     def get(self, request):
-        return Response()
+        return Response({"msg": "get method!"})
 
     # 회원가입
     def post(self, request):
-        user_serializer = UserSerializer(request.data)
+        user_serializer = UserSerializer(data=request.data)
         user_serializer.is_valid(raise_exception=True)
         user_serializer.save()
         return Response(user_serializer.data, status=status.HTTP_201_CREATED)
 
+    # 사용자 정보 수정
     def put(self, request):
-        return Response()
+        return Response({"msg": "put method!"})
 
+    # 사욪자 삭제
     def delete(self, request):
-        return Response()
+        return Response({"msg": "delete method!"})
+
