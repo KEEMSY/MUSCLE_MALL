fc920f927ac39a9e65c053cf1f51f25e7e35d5e6
KEEMSY <phzzz@naver.com>
2022-07-06 13:45:18 +0900

    add: test_get_coach_when_coach_does_not_exist를 통한 get_coach() 함수 추가

--- userapp/permissions/coach_permissions.py
+++ userapp/permissions/coach_permissions.py
@@ -2,36 +2,39 @@ from rest_framework import status
 from rest_framework.exceptions import APIException
 from rest_framework.permissions import BasePermission
 
+from userapp.models import Coach
+
 
 class GenericAPIException(APIException):
     def __init__(self, status_code, detail=None, code=None):
         self.status_code = status_code
         super().__init__(detail=detail, code=code)
 
-
+# 수정이 필요함
 class IsAuthenticatedAndIsAprovedCoach(BasePermission):
     ABAILIBLE_METHODS = ('GET', 'POST', 'DELETE')
     message = '접근 권한이 없습니다.'
 
     def has_permission(self, request, view):
-        user = request.user
+        user_id = request.user.id
+        coach = Coach.objects.get(user_id=user_id)
 
-        if not user.is_authenticated:
+        if not coach.user.is_authenticated:
             response = {
                 "detail": "서비스를 이용하기 위해 로그인 해주세요.",
             }
             raise GenericAPIException(status_code=status.HTTP_401_UNAUTHORIZED, detail=response)
 
-        if not user.approved_user:
+        if not coach.user.approved_user or  not coach.approved_coach:
             response = {
-                "detail": "코치 승인 심사 중 입니다.",
+                "detail": "유저 승인 심사 중 입니다.",
             }
             raise GenericAPIException(status_code=status.HTTP_401_UNAUTHORIZED, detail=response)
 
-        if user.is_authenticated or user.is_admin:
+        if coach.user.is_authenticated or coach.user.is_admin:
             return True
 
-        if user.is_authenticated and request.method in self.ABAILIBLE_METHODS:
+        if coach.user.is_authenticated and request.method in self.ABAILIBLE_METHODS:
             return True
 
         return False
---
+++ userapp/tests/services/test_coach_service.py
@@ -0,0 +1,33 @@
+from django.test import TestCase
+
+from userapp.models import User
+from userapp.services.coach_service import get_coach
+
+
+class TestCoachService(TestCase):
+    coach_data = {
+        "nickname": "WeightKing",
+        "phone_number": "010-1234-1234",
+        "kind": "fitness"
+    }
+
+    def make_user(self):
+        user = User.objects.create(
+            username="test_username1",
+            email="test@email.com",
+            password="1234",
+            fullname="test_fullname",
+            gender="male",
+            approved_user="True"
+        )
+        return user
+
+    def test_get_coach_when_coach_does_not_exist(self):
+        # given
+        user = self.make_user()
+
+        # when
+        coach = get_coach()
+
+        # expect
+        self.assertEqual(0, len(coach))
