43cd2d24e767fb3af4d2dac84e270e77e4e343b4
KEEMSY <phzzz@naver.com>
2022-07-11 23:10:31 +0900

    add: BoardCategory, Board 모델 추가

--- communityapp/models.py
+++ communityapp/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+
 # Create your models here.
+class BoardCategory(models.Model):
+    KIND = {
+        ("Notice", "Notice"),
+        ("free", "Free"),
+    }
+    kind = models.CharField("종류", choices=KIND, null=False, max_length=20)
+    description = models.CharField("설명", max_length=256)
+
+    def __str__(self):
+        return self.kind
+
+
+class Board(models.Model):
+    category = models.ForeignKey(BoardCategory, related_name="category", on_delete=models.SET_NULL, null=True)
+
+    title = models.CharField(max_length=50)
+    content = models.CharField(max_length=250, error_messages={'error': '내용이 너무 깁니다.'})
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
