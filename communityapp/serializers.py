7dad8017f210d8213f38e94f97201caf8ed4373d
KEEMSY <phzzz@naver.com>
2022-07-11 23:22:21 +0900

    add : BoardSerializer 추가

--- communityapp/serializers.py
+++ communityapp/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
-from communityapp.models import BoardCategory
+from communityapp.models import BoardCategory, Board
 
 
 class BoardCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = BoardCategory
         fields = ['kind', 'description']
+
+
+class BoardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Board
+        fields = ['category', 'title', 'content', 'created_at']
