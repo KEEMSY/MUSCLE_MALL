76473dabe5e8a22de40d472def82e883bdfbbe28
KEEMSY <phzzz@naver.com>
2022-07-12 16:43:52 +0900

    update: BoardCategoryApiView에 board_category 수정 추가

--- communityapp/views.py
+++ communityapp/views.py
@@ -5,7 +5,7 @@ from rest_framework import permissions, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from communityapp.services.board_service import get_board_category, save_board_category
+from communityapp.services.board_service import get_board_category, save_board_category, edit_board_category
 
 
 class BoardCategoryApiView(APIView):
@@ -19,8 +19,9 @@ class BoardCategoryApiView(APIView):
         category = save_board_category(**request.data)
         return Response(category, status=status.HTTP_201_CREATED)
 
-    def put(self, request):
-        return Response({'msg': 'put method'})
+    def put(self, request, category_id):
+        category = edit_board_category(category_id, **request.data)
+        return Response(category, status=status.HTTP_200_OK)
 
     def delete(self, request):
         return Response({'msg': 'delete method'})
