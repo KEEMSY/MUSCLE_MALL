63442fea29a12f144760448baf1b3ec995562f5f
KEEMSY <phzzz@naver.com>
2022-07-18 14:54:39 +0900

    add: test_save_board_like_when_board_does_not_exist를 통한 게시글이 없는경우에 대한 에러 확인

--- communityapp/services/board_like_service.py
+++ communityapp/services/board_like_service.py
@@ -1,12 +1,44 @@
+from rest_framework import status
+
+from MM.api_exception import GenericAPIException
+from communityapp.models import BoardLike, Board
 from communityapp.serializers import BoardLikeSerializer
 
 
 def save_board_like(user_id, board_id):
-    board_like = BoardLikeSerializer(data={
-        "user": user_id,
-        "board": board_id
-    })
-    board_like.is_valid(raise_exception=True)
-    board_like.save()
+    try:
+        Board.objects.get(id=board_id)
+        board_like = BoardLikeSerializer(data={
+            "user": user_id,
+            "board": board_id
+        })
+        board_like.is_valid(raise_exception=True)
+        board_like.save()
 
-    return board_like.data
+        return board_like.data
+
+    except Board.DoesNotExist:
+        response = {
+            "detail": "게시글이 존재하지 않습니다."
+        }
+        raise GenericAPIException(status_code=status.HTTP_404_NOT_FOUND, detail=response)
+
+
+def delete_board_like(user_id, board_id):
+    try:
+        board = Board.objects.get(id=board_id)
+        BoardLike.objects.get(user_id=user_id, board_id=board_id).delete()
+        return True
+
+    except BoardLike.DoesNotExist:
+        response = {
+            "detail": "좋아요가 존재하지 않습니다."
+        }
+        raise GenericAPIException(status_code=status.HTTP_404_NOT_FOUND, detail=response)
+
+    except Board.DoesNotExist:
+        response = {
+            "detail": "게시글이 존재하지 않습니다."
+        }
+        raise GenericAPIException(status_code=status.HTTP_404_NOT_FOUND, detail=response)
+
--- communityapp/tests/services/test_board_like_service.py
+++ communityapp/tests/services/test_board_like_service.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 
-from communityapp.models import BoardCategory, Board
-from communityapp.services.board_like_service import save_board_like
+from MM.api_exception import GenericAPIException
+from communityapp.models import BoardCategory, Board, BoardLike
+from communityapp.services.board_like_service import save_board_like, delete_board_like
 from userapp.models import User
 
 
@@ -57,3 +58,29 @@ class TestBoardLikeTest(TestCase):
         # expect
         self.assertEqual(user.id, board_like["user"])
         self.assertEqual(board.id, board_like["board"])
+
+    def test_save_board_like_when_board_does_not_exist(self):
+        # give
+        user = self.make_user()
+        board_id = 9999
+
+        # expect
+        with self.assertRaises(GenericAPIException):
+            board_like = save_board_like(user.id, board_id)
+
+    def test_delete_board_like(self):
+        # give
+        user = self.make_user()
+        board_category = self.make_categories()[0]
+        self.board_data['user'] = user
+        self.board_data['category'] = board_category
+        board = self.make_board(**self.board_data)
+
+        board_like = BoardLike.objects.create(user=user, board=board)
+
+        # when
+        delete_board_like(user.id, board.id)
+
+        # expect
+        with self.assertRaises(BoardLike.DoesNotExist):
+            BoardLike.objects.get(id=board_like.id)
